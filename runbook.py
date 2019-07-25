4ef9b8c3eaeab72efaf7c9a9e7260fc20e26b1a7
yueguoguo <yueguoguo1024@gmail.com>
2019-07-25 08:42:57 +0000

    SCRIPT: update runbooky

--- runbook.py
+++ runbook.py
@@ -1,14 +1,38 @@
 import papermill as pm
-import time
-import sys
-sys.path.append("./recommenders")
+import argparse
 
-INPUT_NOTEBOOK = 'recommenders/notebooks/00_quick_start/sar_movielens.ipynb'
-OUTPUT_NOTEBOOK = 'sar_movielens_output.ipynb'
+# NOTEBOOK parameters
+notebook_parameters = {
+    'TOP_K': 10,
+    'MOVIELENS_DATA_SIZE': '100k'
+}
 
-pm.execute_notebook(INPUT_NOTEBOOK, OUTPUT_NOTEBOOK)
-results = pm.read_notebook(OUTPUT_NOTEBOOK).dataframe.set_index("name")["value"]
 
-print("The run results:")
-for key, value in results.items():
-    print("{}: {}".format(key, value))
+def run_notebook(args):
+    '''Run notebook
+    '''
+    pm.execute_notebook(
+        args.input_notebook, 
+        args.output_notebook, 
+        cwd='recommenders',
+        parameters=notebook_parameters,
+    )
+    results = pm.read_notebook(args.output_notebook).dataframe.set_index("name")["value"]
+
+    for key, value in results.items():
+        space = ' ' * (10 - len(key))
+        print("{0}:{1} {2}".format(key, space, value))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input_notebook_path', type=str, dest='input_notebook', default='recommenders/notebooks/00_quick_start/sar_movielens.ipynb')
+    parser.add_argument('--output_notebook_path', type=str, dest='output_notebook', default='output.ipynb')
+    args = parser.parse_args()
+    
+    run_notebook(args)
+
+
+if __name__ == "__main__":
+    main()
